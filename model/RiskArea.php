47f44bf41195738c1af68ae99722c5f538f6ea01
Worawuth Puntusithsaree <popcorns1udio@gmail.com>
2021-07-29 02:11:10 +0700

    update check status when add risk area 5

--- model/RiskArea.php
+++ model/RiskArea.php
@@ -26,28 +26,33 @@ class RiskArea
     }
 
     public function checkRisk($conn,$lat,$long,$startDate,$endDate){
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$long&radius=1500&key=AIzaSyCXzEl_l-8_eE-tK1W4KQ3wiKT1OoOGq-E",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        $res = json_decode($response);
-        foreach ($res->results as $item){
-            $placeID = $item->place_id;
-            $sql = "UPDATE `tb_timeline` SET status='1' WHERE place_id = '$placeID' and time_checkin BETWEEN '$startDate' and '$endDate'";
-            $result = $conn->query($sql);
-            $this->found = $result;
+//        $curl = curl_init();
+//
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$long&radius=1500&key=AIzaSyCXzEl_l-8_eE-tK1W4KQ3wiKT1OoOGq-E",
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => '',
+//            CURLOPT_MAXREDIRS => 10,
+//            CURLOPT_TIMEOUT => 0,
+//            CURLOPT_FOLLOWLOCATION => true,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_CUSTOMREQUEST => 'GET',
+//        ));
+//
+//        $response = curl_exec($curl);
+//
+//        curl_close($curl);
+//        $res = json_decode($response);
+        $sql = "SELECT tb_timeline.timeline_id FROM tb_timeline
+INNER JOIN tb_riskarea ON tb_riskarea.placeID = tb_timeline.place_id
+WHERE tb_timeline.time_checkin BETWEEN tb_riskarea.startDate and tb_riskarea.endDate";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                $sql = "UPDATE `tb_timeline` SET status='1' WHERE timeline_id = '{$row['timeline_id']}'";
+                $result = $conn->query($sql);
+                $this->found = $result;
+            }
         }
     }
 }
