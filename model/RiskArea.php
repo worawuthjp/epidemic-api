b1232556c519b857632dea775feee0a742fee801
Worawuth Puntusithsaree <popcorns1udio@gmail.com>
2021-07-29 01:30:16 +0700

    update check status when add risk area

--- model/RiskArea.php
+++ model/RiskArea.php
@@ -4,6 +4,7 @@
 class RiskArea
 {
     public $msg;
+    public $found;
 
     public function __construct()
     {
@@ -17,8 +18,35 @@ class RiskArea
         $result = $conn->query($sql);
         if ($result) {
             $this->msg = "success";
+            $this->checkRisk($conn,$lat,$long,$startDate,$endDate);
         }
         return json_encode($this);
 
+    }
+
+    public function checkRisk($conn,$lat,$long,$startDate,$endDate){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$long&radius=1500&key=AIzaSyCXzEl_l-8_eE-tK1W4KQ3wiKT1OoOGq-E",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $res = $response->result;
+        foreach ($res as $item){
+            $placeID = $item->place_id;
+            $sql = "UPDATE `tb_timeline` SET status='1' WHERE place_id = '$placeID' and timeline_checkin >= '$startDate' and timeline_checkout <= '$endDate' ";
+            $result = $conn->query($sql);
+            $this->found = $result;
+        }
     }
 }
