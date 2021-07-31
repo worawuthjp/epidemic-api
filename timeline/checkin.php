d3b0cdc276e27800b3c560848b4266d8ebfc51a6
Worawuth Puntusithsaree <popcorns1udio@gmail.com>
2021-07-31 15:14:54 +0700

    add get timeline

--- timeline/checkin.php
+++ timeline/checkin.php
@@ -6,25 +6,44 @@ $lat = null;
 $long = null;
 $name = '';
 $placeID = '';
-if(isset($_POST['userID'])){
-    $id = $_POST['userID'];
-}
-
-if(isset($_POST['lat'])){
-    $lat = $_POST['lat'];
-}
-
-if(isset($_POST['long'])){
-    $long = $_POST['long'];
-}
-
-if(isset($_POST['placeName'])){
-    $name = $_POST['placeName'];
-}
-if(isset($_POST['placeID'])){
-    $placeID = $_POST['placeID'];
-}
-
 
 $res = new Checkin();
-echo $res->mapRequest($id,$lat,$long,$name,$placeID,$conn);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST['userID'])){
+        $id = $_POST['userID'];
+    }
+
+    if(isset($_POST['lat'])){
+        $lat = $_POST['lat'];
+    }
+
+    if(isset($_POST['long'])){
+        $long = $_POST['long'];
+    }
+
+    if(isset($_POST['placeName'])){
+        $name = $_POST['placeName'];
+    }
+    if(isset($_POST['placeID'])){
+        $placeID = $_POST['placeID'];
+    }
+    echo $res->mapRequest($id,$lat,$long,$name,$placeID,$conn);
+}else if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    if(isset($_GET['userID'])){
+        $userID = $_GET['userID'];
+    }
+    $row = $res->getLastest($userID,$conn);
+    $response = array();
+    $response['msg'] = 'error';
+    $response['statusCode'] = 200;
+    if($userID){
+        $response['msg'] = 'success';
+        $response['isCheckout'] = $res->isCheckout($row['timeline_id'],$conn);
+        $response['placeID'] = $row['placeID'];
+        $response['placeName'] = $row['placeName'];
+        $response['time_checkin'] = $row['time_checkin'];
+        $response['statusCode'] = 200;
+    }
+    echo json_encode($response);
+
+}
