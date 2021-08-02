3bb75f22e6ddd363550b00c5dcda741c776dee9f
Worawuth Puntusithsaree <popcorns1udio@gmail.com>
2021-08-02 12:48:40 +0700

    timezone

--- conn.php
+++ conn.php
@@ -7,9 +7,10 @@ $servername = "localhost";
 $username = "mypos";
 $password = "Jay_0925191680";
 $db = "db_covid";
-
+date_default_timezone_set("Asia/Bangkok");
 $conn = mysqli_connect($servername, $username, $password, $db);
 $conn->set_charset('utf8');
+
 
 if (!$conn) {
     die("Connection Failed : " . mysqli_connect_error());
