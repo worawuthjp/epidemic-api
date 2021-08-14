2768b9140115b3eded95ba48bac0cb3f1a5e0e17
Phassawich Chomthip <plamche321456@gmail.com>
2021-08-14 23:09:36 +0700

    api

--- constant.php
+++ constant.php
@@ -1,6 +1,6 @@
 <?php
 $API_MAP_KEY = "AIzaSyCLziM9n8DztnZ5oNLznzyi9XYKnjhsWYE";
-$SIGNUPLINK = "http://localhost/ConnectDBProject/connectApp/verify.php?id=";
-$RESETPATH = "http://localhost/epidemic/reset-password.php?id=";
+$SIGNUPLINK = "http://localhost/epidemic-api/verify.php?id=";
+$RESETPATH = "http://localhost/web-admin-epidemic/reset-password.php?id=";
 $SYSEMAIL = "jay.worawuth@gmail.com";
 $SYSPASS = "rxtxhldumrvbgqzq";
